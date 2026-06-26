41ee8eb8609ba92b3b398c99fe34786a90d0193b
ETN-Villain <idontdocrypto@outlook.com>
2026-06-26 15:26:15 +0100

    Add real App import

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
 import './index.css'
-
-function App() {
-  return <div>Testing Vercel Build</div>
-}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
