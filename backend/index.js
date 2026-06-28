e7258d74b0cec155ba576f860bc5173f2a44f75d
ETN-Villain <idontdocrypto@outlook.com>
2026-06-28 21:14:35 +0100

    G

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import generateNftRouter from "./utils/generateNft.js";
+import generateNftRouter from "./utils/GenerateNft.js";
 
 dotenv.config();
 
